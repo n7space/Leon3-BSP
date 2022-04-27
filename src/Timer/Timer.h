7daf68b8637b4488884de01883317bbf89f129a0
Julia Aniecka <janiecka@n7space.com>
2022-04-27 17:53:16 +0200

    Add function prototypes and API
    Signed-off-by: Julia Aniecka <janiecka@n7space.com>

--- src/Timer/Timer.h
+++ src/Timer/Timer.h
@@ -32,6 +32,47 @@
 #ifndef BSP_TIMER_H
 #define BSP_TIMER_H
 
+#include <TimerRegisters.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+/// \brief Timer device identifiers.
+typedef enum
+{
+    Timer_Id_0 = 0,   ///< Base system timer
+    Timer_Id_1 = 1,   ///< Timer 1
+    Timer_Id_2 = 2,   ///< Timer 2
+    Timer_Id_3 = 3,   ///< Timer 3
+    Timer_Id_4 = 4,   ///< Timer 4
+    Timer_Id_Max = 5, ///< Error value
+} Timer_Id;
+
+/// \brief Timer configuration descriptor.
+typedef struct
+{
+    uint32_t clockSource;    ///< Clock source
+    bool isInterruptEnabled; ///< Is interrupt enabled
+    bool isEnabled;          ///< Is timer enabled
+    uint32_t reloadValue;    ///< Reload value
+} Timer_Config;
+
+/// \brief Timer device descriptor.
+typedef struct
+{
+    Timer_Id id;           ///< Timer device id
+    TimerRegisters_t regs; ///< Hardware timer registers
+} Timer;
+
+/// \brief Configures an Timer device based on a configuration descriptor.
+/// \param [in] timer Timer device descriptor.
+/// \param [in] config A configuration descriptor.
+void Timer_setConfig(Timer* const timer, const Timer_Config* const config);
+
+/// \brief Retrieves configuration of an Timer device.
+/// \param [in] timer Timer device descriptor.
+/// \param [out] config A configuration descriptor.
+void Timer_getConfig(const Timer* const timer, Timer_Config* const config);
+
 #endif // BSP_TIMER_H
 
 /** @} */
